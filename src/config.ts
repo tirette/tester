0635af183d83122e45ca8e39a2ed106225483680
Luïs De Zutter <luzedz@gmail.com>
2020-11-14 13:46:50 +0100

    fix(scope): Fix .jest folder not being found

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ import internal from './utils/internal';
 module.exports = {
   rootDir: local(''),
   moduleDirectories: [local('src'), local('node_modules')],
-  setupFiles: [internal('../.jest/registerContext.js')],
+  setupFiles: [internal('./utils/registerContext.js')],
   transform: {
     '^.+\\.[jt]sx?$': ['babel-jest', { configFile: internal('babelConfig.js') }],
-    '^.+\\.(svg|pdf|jpg|png)$': internal('../.jest/fileTransform.js'),
+    '^.+\\.(svg|pdf|jpg|png)$': internal('./utils/fileTransform.js'),
   },
 };
